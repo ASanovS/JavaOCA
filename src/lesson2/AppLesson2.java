06ddee68eaf5e8cc4eea4cb98aff2e977cd7d91f
Serhii Asanov <serge.asanov@gmail.com>
2022-04-08 23:22:02 +0300

    finish hw lesson2

--- src/lesson2/AppLesson2.java
+++ src/lesson2/AppLesson2.java
@@ -2,6 +2,58 @@ package lesson2;
 
 public class AppLesson2 {
     public static void main(String[] args) {
+      
+        byte b1 = 12; 
+        byte b2 = 0B1100; 
+        byte b3 = 014; 
+        byte b4 = 0xC; 
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+
+        short s1 = 1300;
+        short s2 = 0B10100010100;
+        short s3 = 02424; 
+        short s4 = 0x514; 
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0; 
+        int i2 = 0B0000; 
+        int i3 = 00; 
+        int i4 = 0x0;
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long l1 = 123456789;
+        long l2 = 0B111010110111100110100010101;
+        long l3 = 0726746425;
+        long l4 = 0x75BCD15;
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+        
+        float f1 = 3.14F;
+        float f2 = 20;
+        System.out.println(f1);
+        System.out.println(f2);
+
+        double d1 = 3.14;
+        double d2 = 20;
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+        System.out.println(bool1);
+        System.out.println(bool2);
         
     }
 }
