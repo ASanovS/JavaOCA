d9ddef15d85647d41d1700a54d793e2c675c642a
Serhii Asanov <serge.asanov@gmail.com>
2022-04-08 18:47:15 +0300

    lesson1 compleat hw

--- src/lesson1/App.java
+++ src/lesson1/App.java
@@ -2,6 +2,12 @@ package lesson1;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello lesson 1");
+        System.out.println("               РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                              О. Хайям");
+
     }
 }
