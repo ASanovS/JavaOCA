7b24f1734af50c85ce1c8dd1ce8298b0dc11ecd9
Sergii Asanov <serge.asanov@gmail.com>
2022-05-16 23:39:12 +0300

    lesson8 task1

---
+++ src/lesson8/App.java
@@ -0,0 +1,11 @@
+package lesson8;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Task1.umnozhenije(2, 3, 4));
+        Task1.delenie(9, 2);
+        System.out.println(Task1.umnozhenije(2.5, 3.5, 4.5));
+        Task1.delenie(15, 4);
+    }
+}
+
---
+++ src/lesson8/Task1.java
@@ -0,0 +1,11 @@
+package lesson8;
+
+public class Task1 {
+    static double umnozhenije(double a, double b, double c) {
+        return a * b * c;
+    }
+
+    static void delenie(int a, int b) {
+        System.out.println("Celoe chislo = " + a / b + "; ostatok = " + a % b);
+    }
+}
