27eab618b961c7dc8a75ba163fedca836e21bb0b
Serhii Asanov <serge.asanov@gmail.com>
2022-09-12 19:52:43 +0300

    initial lesson14

---
+++ src/lesson14/App.java
@@ -0,0 +1,10 @@
+package lesson14;
+
+public class App {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Lesson 14");
+        }
+    }
+}
