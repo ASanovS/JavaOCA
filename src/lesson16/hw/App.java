83df01a00e6960ca648efb832d8687eed7fff161
Serhii Asanov <serge.asanov@gmail.com>
2022-09-16 12:33:03 +0300

    initial hw16

---
+++ src/lesson16/hw/App.java
@@ -0,0 +1,26 @@
+package lesson16.hw;
+
+/*
+ * Создайте класс, в котором создайте метод email.
+ * Данный метод принимает в метод 1 String параметр.
+ * Данный параметр должен содержать в себе email в 
+ * следющем виде: ya@yahoo.com; gog@gmail.com; ua@ukr.net;
+ * т.е. после каждого email должен стоять знак препинания ";".
+ * Ваш метод должен выводить информацию о том, какой почтой
+ * пользуются, исходя из параметра.
+ * Оутпут должен быть следующего вида:
+ * yahoo
+ * gmail
+ * ukr
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+    
+    static String email(String inp) {
+        
+        return null;
+    }
+}
