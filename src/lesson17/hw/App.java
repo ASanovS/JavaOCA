0112f1086dc8cb9c456dc16a27668dc5b5111bf8
Serhii Asanov <serge.asanov@gmail.com>
2022-09-17 20:26:04 +0300

    hw17 finished

--- src/lesson17/hw/App.java
+++ src/lesson17/hw/App.java
@@ -9,16 +9,28 @@ package lesson17.hw;
 
 public class App {
     public static void main(String[] args) {
-        
-        StringBuilder sb2 = new StringBuilder("Hello");
+
         StringBuilder sb1 = new StringBuilder("Hello");
-        StringBuilder sb3 = sb2;
-        
+        StringBuilder sb2 = new StringBuilder("Hello");
+        StringBuilder sb3 = new StringBuilder("hello");
+        StringBuilder sb4 = sb3;
+
         System.out.println(ravenstvo(sb1, sb2));
         System.out.println(ravenstvo(sb2, sb3));
+        System.out.println(ravenstvo(sb4, sb3));
+        System.out.println(ravenstvo(new StringBuilder(""), new StringBuilder("")));
+        System.out.println(ravenstvo(new StringBuilder(), new StringBuilder()));
     }
 
     static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2) {
-        return sb1 == sb2;
+
+        if (sb1.length() == sb2.length()) {
+            for (int i = 0; i < sb1.length(); i++) {
+                if (sb1.charAt(i) != sb2.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
